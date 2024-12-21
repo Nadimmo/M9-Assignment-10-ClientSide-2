b98bf83e1a65f22d22fe5d6d2a93757d63a88959
nadimmo <nadimmostafa334@gmail.com>
2024-12-21 16:35:45 +0600

    update south asia turist sport

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,14 +18,13 @@ import logo2 from "../../assets/image/bandarban2.jpg";
 import logo3 from "../../assets/image/ragamati2.jpg";
 import logo4 from "../../assets/image/sentmartin.jpg";
 import logo5 from "../../assets/image/tiger.jpg";
-import world from '../../assets/image/world.webp'
 
 import "./style.css";
 import Cart from "./Cart";
 
 const Home = () => {
   const data = useLoaderData();
-  console.log(data);
+  // console.log(data);
 
   return (
     <div>
@@ -89,86 +88,40 @@ const Home = () => {
         </h1>
         <br />
         <br />
-        <div className="  ml-12 lg:ml-14 grid grid-cols-1 gap-10 lg:grid-cols-3">
-          <div className="w-[280px] h-[280px] opacity-[0.4] hover:opacity-[0.9]">
-            <img
-              src="https://i.ibb.co/Pz554By/Bangladesh.jpg"
-              alt=""
-              className="absolute  w-[280px] h-[280px]  hover:shadow-2xl "
-            />
-            <h1 className="text-2xl font-bold text-center text-white relative mt-28">
-              Bangladesh
-            </h1>
-            <p className="relative mt-16 ml-16 btn btn-info">
-              <Link to="/bd"> All Tourist Spots</Link>{" "}
-            </p>
-          </div>
-          <div className="w-[280px] h-[280px] opacity-[0.4] hover:opacity-[0.9]">
-            <img
-              src="https://i.ibb.co/PmzGPHs/malaysia.jpg"
-              alt=""
-              className="absolute w-[290px] h-[280px]  hover:shadow-2xl "
-            />
-            <h1 className="text-2xl font-bold text-center text-white relative pt-28">
-              Malaysia
-            </h1>
-            <p className="relative mt-16 ml-16 btn btn-info">
-              <Link to="/malaysia"> All Tourist Spots</Link>{" "}
-            </p>
-          </div>
-          <div className="w-[280px] h-[280px] opacity-[0.4] hover:opacity-[0.9]">
-            <img
-              src="https://i.ibb.co/0J63nhW/indonasia.jpg"
-              alt=""
-              className="absolute w-[280px] h-[280px]  hover:shadow-2xl "
-            />
-            <h1 className="text-2xl font-bold text-center text-white relative pt-28">
-              Indonesia
-            </h1>
-            <p className="relative mt-16 ml-16 btn btn-info">
-              <Link to="/indonasia"> All Tourist Spots</Link>{" "}
-            </p>
-          </div>
-          <div className="w-[280px] h-[280px] opacity-[0.4] hover:opacity-[0.9]">
-            <img
-              src="https://i.ibb.co/d49NWTG/vietnam.jpg"
-              alt=""
-              className="absolute w-[280px] h-[280px]  hover:shadow-2xl "
-            />
-            <h1 className="text-2xl font-bold text-center text-white  relative pt-28">
-              Vietnam
-            </h1>
-            <p className="relative mt-16 ml-16 btn btn-info">
-              <Link to="/vietnam"> All Tourist Spots</Link>{" "}
-            </p>
-          </div>
-          <div className="w-[280px] h-[280px] opacity-[0.4] hover:opacity-[0.9]">
-            <img
-              src="https://i.ibb.co/h7mr15J/cambodia.jpg"
-              alt=""
-              className="absolute w-[280px] h-[280px]  hover:shadow-2xl "
-            />
-            <h1 className="text-2xl font-bold text-center text-white  relative pt-28">
-              Cambodia
-            </h1>
-            <p className="relative mt-16 ml-16 btn btn-info">
-              <Link to="/combodia"> All Tourist Spots</Link>{" "}
-            </p>
-          </div>
-          <div className="w-[280px] h-[280px] opacity-[0.4] hover:opacity-[0.9]">
-            <img
-              src="https://i.ibb.co/4TY4HX2/thailand.jpg"
-              alt=""
-              className="absolute w-[280px] h-[280px]  hover:shadow-2xl "
-            />
-            <h1 className="text-2xl font-bold text-center text-white  relative pt-28">
-              Thailand
-            </h1>
-            <p className="relative mt-16 ml-16 btn btn-info">
-              <Link to="/thailand"> All Tourist Spots</Link>{" "}
-            </p>
-          </div>
+        <div className="container mx-auto px-4 py-8 grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
+          {[
+            { name: "Bangladesh", image: "https://i.ibb.co.com/Pz554By/Bangladesh.jpg", link: "/bd" },
+            { name: "Malaysia", image: "https://i.ibb.co.com/PmzGPHs/malaysia.jpg", link: "/malaysia" },
+            { name: "Indonesia", image: "https://i.ibb.co.com/0J63nhW/indonasia.jpg", link: "/indonasia" },
+            { name: "Vietnam", image: "https://i.ibb.co.com/d49NWTG/vietnam.jpg", link: "/vietnam" },
+            { name: "Cambodia", image: "https://i.ibb.co.com/h7mr15J/cambodia.jpg", link: "/combodia" },
+            { name: "Thailand", image: "https://i.ibb.co.com/4TY4HX2/thailand.jpg", link: "/thailand" },
+          ].map((country, index) => (
+            <div
+              key={index}
+              className="relative w-full h-[280px] rounded-lg overflow-hidden group transform hover:scale-105 transition-transform duration-300 shadow-lg hover:shadow-2xl"
+            >
+              <img
+                src={country.image}
+                alt={country.name}
+                className="w-full h-full object-cover opacity-70 group-hover:opacity-100 transition-opacity duration-300"
+              />
+              <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent group-hover:from-indigo-900 transition-all duration-300"></div>
+              <h1 className="absolute bottom-12 left-1/2 transform -translate-x-1/2 text-2xl font-bold text-white text-center group-hover:text-yellow-300 transition-colors duration-300">
+                {country.name}
+              </h1>
+              <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2">
+                <Link
+                  to={country.link}
+                  className="px-4 py-2 bg-indigo-600 text-white font-semibold rounded-lg hover:bg-indigo-700 transition-colors duration-300"
+                >
+                  All Tourist Spots
+                </Link>
+              </div>
+            </div>
+          ))}
         </div>
+
       </div>
       {/* explore world */}
       <div className="mt-10">
@@ -176,14 +129,14 @@ const Home = () => {
           <div className="hero lg:h-[500px]  bg-black p-5">
             <div className="hero-content flex-col lg:flex-row-reverse">
               <img
-                src={world}
+                src='https://i.ibb.co.com/jVrYNWS/world.webp'
                 className=" lg:h-[400px] lg:w-[500px] rounded-lg shadow-2xl"
               />
               <div>
                 <h1 className="text-4xl font-extrabold text-white">Explore the World</h1>
                 <h3 className="text-2xl font-extrabold mt-2 text-white">Discover Our Trips</h3>
                 <p className="py-6 text-white">
-                We travel to incredible, often overlooked places. Our global community of local experts grants you access to unique, small-group experiences.
+                  We travel to incredible, often overlooked places. Our global community of local experts grants you access to unique, small-group experiences.
                 </p>
                 <button className="btn bg-black text-white font-bold">View all trips</button>
               </div>
@@ -192,15 +145,15 @@ const Home = () => {
           <div className="bg-sky-400 text-center p-10">
             <h1 className="text-4xl font-bold text-white">Subscribe to our mailing list</h1>
             <h3 className="text-2xl mt-2 text-white">BE THE FIRST TO GET LATEST NEWS AND OFFERS!</h3>
-          <div className="mt-5">
-            <input className="h-[40px] p-5" type="email" name="" id="" placeholder="Enter Your Email" />
-            <button className="btn bg-black text-white lg:ml-2">Subscribe</button>
-          </div>
+            <div className="mt-5">
+              <input className="h-[40px] p-5" type="email" name="" id="" placeholder="Enter Your Email" />
+              <button className="btn bg-black text-white lg:ml-2">Subscribe</button>
+            </div>
           </div>
         </di>
       </div>
-       {/* join our community */}
-       <div className="mt-10 bg-base-200 text-black p-10 lg:h-[400px]">
+      {/* join our community */}
+      <div className="mt-10 bg-base-200 text-black p-10 lg:h-[400px]">
         <div className="text-center">
           <p className="text-sm">JOIN THE COMMUNITY</p>
           <h1 className="text-2xl font-bold mt-2 ">The Atlas is always evolving thanks to our community Here’s how to get involved:</h1>
@@ -218,7 +171,7 @@ const Home = () => {
             <h3 className="text-xl font-bold mt-2 ">Become a Member</h3>
             <p className="text-sm py-3">Join our community to see far fewer ads, get a members-only newsletter, receive credit toward your next Atlas Obscura trip, and more.
 
-</p>
+            </p>
             <p className="text-orange-500 font-bold"><a href="">Explore The Benefits</a></p>
           </div>
           <div className="lg:w-[340px]  bg-base-100 p-5 text-center">
@@ -228,7 +181,7 @@ const Home = () => {
             <p className="text-orange-500 font-bold"><a href="">Download The App</a></p>
           </div>
         </div>
-       </div>
+      </div>
     </div>
   );
 };
