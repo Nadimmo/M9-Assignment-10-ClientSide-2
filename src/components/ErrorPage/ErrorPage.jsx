b3a2267e4664cfe161d05074afad46ed6c169358
nadimmo <nadimmostafa334@gmail.com>
2024-12-21 15:17:28 +0600

    add error page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,7 @@
-import logo from './404.gif'
 const ErrorPage = () => {
     return (
         <div className='mt-[40%] lg:mt-0 lg:ml-[10%] lg:mr-[10%]'>
-            <img src={logo} alt="" className='w-[100%]' />
+            <img src="https://ibb.co.com/6B8qY68" alt="" className='w-[100%]' />
         </div>
     );
 };
